7ce3ec1d05ecbd3c8b116cb9c788e38452c90383
ahmed-gamal-mohamed-al-araby <66698440+ahmed-gamal-mohamed-al-araby@users.noreply.github.com>
2022-05-28 15:46:55 +0200

    Add files via upload

---
+++ 503.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>System Maintenance</title>
+    <style>
+        .container {
+            padding-top: 100px;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div class="container text-center">
+    <img src="{{asset("assets/images/images-2.jpg")}}" class="" width="20%" alt="">
+    <h1 class="text-center">Site Under Maintenance</h1>
+    <p class="text-center">We are currently under maintenance. we will be back shortly , thank you.</p>
+</div>
+</body>
+</html>
